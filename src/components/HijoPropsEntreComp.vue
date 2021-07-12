b26b230b44e6d21e8d7fcd2c5a1bb10c4446d1d8
Robert de la Fuente <robert@climatetrade.com>
2021-07-12 19:49:57 +0200

    Added 'sending props beetwen components' documentation

---
+++ src/components/HijoPropsEntreComp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h4>
+      Componente Hijo
+    </h4>
+    <p>Potencia: {{ power }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    power: {
+      type: Number,
+      default: 60,
+    },
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/PropsEntreComponentes.vue
+++ src/views/PropsEntreComponentes.vue
@@ -1,11 +1,46 @@
 <template>
   <div>
     <h2>Pasando Props entre Componentes</h2>
+    <p>
+      Los props es simple información que se pasa de un componente a otro, suele
+      ser de un componente padre al componente hijo. El componente padre enviará
+      una variable y y componente hijo recibira esa variable mediante los props.
+    </p>
+    <br />
+    <h4>
+      Componente Padre
+    </h4>
+    <p>
+      Enviando informacion de la variable power, en este caso 30, al componente
+      hijo...
+    </p>
+    <HijoPropsEntreComp :power="power" />
+    <br />
+    <h4>
+      Imágen Componente Padre
+    </h4>
+    <img src="../assets/PropsEntreComp1.png" alt="Componente Padre" />
+    <h4>
+      Imágen Componente Hijo
+    </h4>
+    <img src="../assets/PropsEntreComp2.png" alt="Componente Hijo" />
   </div>
 </template>
 
 <script>
-export default {};
+import HijoPropsEntreComp from "../components/HijoPropsEntreComp.vue";
+export default {
+  components: {
+    HijoPropsEntreComp,
+  },
+  setup() {
+    let power = 30;
+
+    return {
+      power,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
